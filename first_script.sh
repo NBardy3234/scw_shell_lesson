ba7ef9360a27cf9b43eb39eb54f164bb2a3ec620
Nicole Bardabelias <nmb23@dhcp-10-132-142-44.uawifi.arizona.edu>
2017-02-25 12:03:51 -0700

    Add comment to the script

--- first_script.sh
+++ first_script.sh
@@ -1,8 +1,13 @@
+# This script is used to count the number of characters in a file
+
 # $1 is input file on command line
 #head -5 $1 | sort > temp.txt
+#head -5 = print first 5 lines of file
+# > [filename] = dump output into this file
 
 # $2 is output file on command line
 #wc -c temp.txt > $2
+#wc = print newline, word, & byte counts, -c = print byte counts
 
 #We don't need this intermediate file, delete
 #rm temp.txt
