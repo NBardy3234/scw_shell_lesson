673ee8ab2630a3a81e00e54055d19673b0cf74ad
Nicole Bardabelias <nmb23@dhcp-10-132-142-44.uawifi.arizona.edu>
2017-02-25 12:18:47 -0700

    Edit help function in script

--- first_script.sh
+++ first_script.sh
@@ -1,7 +1,7 @@
 # This script is used to count the number of characters in a file
 
 # We're adding a help function!
-if [ "$1" == "-h" ]; then
+if [ "$1" == "-h" ]||[ "$1" == "--help" ]; then
     echo "Usage: sh $0 <input_file> <output_file>"
     exit
 fi
